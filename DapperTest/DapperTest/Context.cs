ce20ba7afa031aa680857bc0cc9dd22a62ccf25d
Rider2 <kompakt5@o2.pl>
2021-09-01 18:35:44 +0200

    first

---
+++ DapperTest/DapperTest/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DapperTest
+{
+    class Context: DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        private string _connectionString;
+        public Context()
+        {
+            _connectionString = "DataSource=c:\\DPersonsDB.sqlite";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("DataSource=d:\\DPersonsDB.sqlite.:memdb1?mode=memory&cache=shared");
+    }
+}
---
+++ DapperTest/DapperTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+
+namespace DapperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cs = "DataSource=d:\\DPersonsDB.sqlite.:memdb1?mode=memory&cache=shared";
+
+            using var con = new SqliteConnection(cs);
+            con.Open();
+            List<Person> persons = con.Query<Person>("select * from Persons").AsList<Person>();
+
+
+
+            foreach (Person person in persons)
+            {
+                Console.WriteLine("{0} {1}", person.FirstName, person.LastName);
+            }
+            //Person entity = new Person
+            //{
+            //    FirstName = "Staszek",
+            //    LastName = "Stefczyk"
+            //};
+            //string sqlQuery = "INSERT INTO [Persons]([FirstName],[LastName]) VALUES (@FirstName,@LastName)";
+            //con.Execute(sqlQuery,
+            //    new
+            //    {
+            //        entity.FirstName,
+            //        entity.LastName,
+            //    });
+
+            //con.Close();
+        }
+    }
+}
